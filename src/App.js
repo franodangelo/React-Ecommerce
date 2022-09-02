fdc857b1c2107939338023038a16a740e67e1384
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-09-02 15:18:28 -0300

    container imported

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import ItemListContainer from "./components/ItemListContainer";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
+      <ItemListContainer greeting='Hey! I am working in my e-commerce right now'/>
     </>
   );
 }
