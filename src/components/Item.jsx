54e4bd71cbba345e2d02e3295098f2ac11b2133c
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-09-07 23:05:19 -0300

    counter ok

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import ItemCount from './ItemCount';
 
-export default function Item({ name, thumbnail, category, description, price, realStock }) {
+export default function Item({ name, thumbnail, category, description, rate, price, realStock }) {
     return (
-        <main className='max-w-[400px] h-[400px] p-4 rounded-lg bg-emerald-600/50'>
-            <div>
-                <div className='flex'>
-                    <img className='' src={thumbnail} alt="product img" />
-                </div>
-                <section className='flex flex-col'>
-                    <h2 className='font-thin uppercase'>{category}</h2>
-                    <div className='flex justify-between items-center'>
-                        <h1 className='py-2 font-bold text-2xl'>{name}</h1>
-                        <span className='font-semibold text-lg'>U$S {price}</span>
+        <div class="w-full max-w-sm rounded-lg shadow-md bg-emerald-900">
+            <img class="p-8 rounded-t-lg" src={thumbnail} alt="product img" />
+            <div class="px-5 pb-5">
+                <div className='flex justify-between'>
+                    <h5 class="text-xl font-semibold text-white">{name}</h5>
+                    <div className='flex'>
+                        <span class="flex items-center bg-emerald-100 text-emerald-800 text-xs font-semibold px-2 py-1 rounded">
+                            <svg aria-hidden="true" class="w-4 h-4 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                            </svg>
+                            {rate}
+                        </span>
                     </div>
-                    <p className='w-40 text-sm'>"{description}"</p>
-                </section>
-                <section className='mt-8 flex flex-col items-center gap-4'>
-                    <h2 className='font-thin text-sm uppercase'>Units available: {realStock}</h2>
-                    <ItemCount />
-                </section>
+                </div>
+                <div class="flex items-center justify-between mt-2.5 mb-5">
+                    <h1 className='text-white'>Units available: {realStock}</h1>
+                    <span class="text-3xl font-bold text-gray-900 dark:text-white">${price}</span>
+                </div>
+                <div class="flex justify-between items-center">
+                    <ItemCount stock={realStock} initial={0} />
+                </div>
             </div>
-        </main>
+        </div>
+
     )
 }
--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,40 +1,47 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-export default function ItemCount({ stock, initial }) {
-
+export default function ItemCount({ stock, initial, cartItems }) {
     const [count, setCount] = useState(initial);
-    const [newStock, setNewStock] = useState(stock);
+    // const [newStock, setNewStock] = useState(stock);
+    const [newCartItems, setNewCartItems] = useState(cartItems);
 
-    const addUnit = () => {
-        if (count === newStock) setCount(newStock);
-        setCount(newCount => newCount++);
+    useEffect(() => {
+        setCount(initial);
+    }, [initial])
+
+    function addUnit() {
+        if (count < stock) setCount(count + 1);
     }
 
-    const subtractUnit = () => {
-        if (count === initial) setCount(1);
-        if (count > newStock) setCount(0);
-        setCount(newCount => newCount--);
-    }
-
-    const addToCart = () => {
-        if (newStock >= count) {
-            alert(`You added ${count} items to your shopping cart!`);
-            setNewStock(newStock => newStock - count);
-            setCount(0);
-        } else {
-            alert(`You're trying to add more items than the real stock`)
-            setCount(0);
+    function subtractUnit() {
+        if (count > initial) {
+            setCount(count - 1);
         }
+    }
+
+    function addToCart() {
+        if (count === 1) {
+            alert(`You added 1 item to the cart`);
+        } else if (count > initial && count < stock) {
+            alert(`You added ${count} items to the cart`);
+        }
+        setNewCartItems(newCartItems + count);
     }
 
     return (
         <div className='flex flex-col w-full justify-between items-center gap-4'>
-            <section className='flex gap-8'>
-                <button className='h-8 w-8 justify-center rounded-full font-bold text-emerald-700 bg-emerald-200 border-2 border-emerald-700' onClick={addUnit}>-</button>
-                <h1 className='flex items-center mx-2 px-4'>{count}</h1>
-                <button className='h-8 w-8 justify-center rounded-full font-bold text-emerald-700 bg-emerald-200 border-2 border-emerald-700' onClick={subtractUnit}>+</button>
+            <section className='flex gap-2'>
+                <button className='h-8 w-8 justify-center rounded-full font-bold text-emerald-700 bg-emerald-200 border-2 border-emerald-700'
+                    onClick={subtractUnit}>-
+                </button>
+                <h1 className='flex items-center mx-2 px-4 bg-white rounded'>{count}</h1>
+                <button className='h-8 w-8 justify-center rounded-full font-bold text-emerald-700 bg-emerald-200 border-2 border-emerald-700'
+                    onClick={addUnit}>+
+                </button>
             </section>
-            <button className='flex justify-center text-center items-center px-4 py-2 font-bold uppercase text-emerald-100 rounded-full bg-emerald-700' onClick={addToCart}>Add to cart</button>
+            <button className='flex justify-center text-center items-center px-4 py-2 font-bold uppercase text-emerald-100 rounded-full bg-emerald-700'
+                onClick={addToCart}>Add to cart
+            </button>
         </div>
     )
 }
