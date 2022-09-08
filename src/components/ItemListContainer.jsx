77b544078edef51e1809c456e4b2568221936e7d
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-09-07 23:02:30 -0300

    thumbnail and rates

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -3,20 +3,22 @@ import Item from './Item';
 
 export default function ItemListContainer() {
     return (
-        <div className='flex w-full h-screen p-12 gap-8 justify-center items-center'>
+        <div className='flex w-full p-8 gap-8 items-center'>
             <Item
                 thumbnail={`https://cdnmedia.racquets4u.com/media/catalog/product/cache/6e8e24cc73e2eff11526e72b5f24c033/w/i/wilson-championship-3-tennis-balls.jpg`}
                 name={"Tennis balls"}
                 category={"sports"}
                 description={"3-ball can recommended for all surfaces"}
+                rate={4.8}
                 price={5.49}
                 realStock={25}
             />
             <Item
-                thumbnail='https://www.e-tennis.com/babolat-pure-aero-rafa-tennis-bag-x-12-751215.html'
+                thumbnail={`https://cdnmedia.racquets4u.com/media/catalog/product/cache/6e8e24cc73e2eff11526e72b5f24c033/w/i/wilson-championship-3-tennis-balls.jpg`}
                 name={"Tennis bag"}
                 category={"sports"}
                 description={"9-racquets capacity"}
+                rate={4.6}
                 price={59.99}
                 realStock={10}
             />
