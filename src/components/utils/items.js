b833c477593fe080024ed3a3379353cd7555f7a3
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-09-09 18:25:01 -0300

    creating the intern catalog

---
+++ src/components/utils/items.js
@@ -0,0 +1,15 @@
+const items = [
+    {
+        id: 1,
+        name: "",
+        category: "",
+        description: "",
+        thumbnail: "",
+        rate: 1,
+        price: 1,
+        discount: 2,
+        stock: 1,
+        fees: 9,
+        freeShipping: true,
+    }
+]
