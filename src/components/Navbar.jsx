979d24bae4308abdc44ef88828eed4e152244caf
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-09-14 16:33:48 -0300

    min fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,10 @@ import CartWidget from './CartWidget';
 
 export default function Navbar({ cartItems }) {
     return (
-        <nav className='flex w-full h-20 px-8 justify-between items-center bg-emerald-300'>
-            <h1 className='font-bold text-xl text-emerald-900 uppercase'>Wlsn</h1>
+        <nav className='flex w-full h-20 px-8 justify-between items-center bg-rose-300'>
+            <h1 className='font-bold text-xl text-rose-900 uppercase'>Wlsn</h1>
             <section>
-                <ul className='flex justify-between gap-8 text-emerald-900'>
+                <ul className='flex justify-between gap-8 text-rose-900'>
                     <li className='font-semibold text-sm uppercase cursor-pointer hover:font-bold'>Pro Staff</li>
                     <li className='font-semibold text-sm uppercase cursor-pointer hover:font-bold'>Blade</li>
                     <li className='font-semibold text-sm uppercase cursor-pointer hover:font-bold'>Clash</li>
@@ -14,8 +14,8 @@ export default function Navbar({ cartItems }) {
                 </ul>
             </section>
             <div className='flex items-center justify-between gap-4'>
-                <button className='px-4 py-2 rounded-full font-bold uppercase text-emerald-700 bg-emerald-100 border-2 border-emerald-700'>Sign in</button>
-                <button className='px-4 py-2 rounded-full font-bold uppercase text-emerald-100 bg-emerald-700'>Sign up</button>
+                <button className='px-4 py-2 rounded-full font-bold uppercase text-rose-700 bg-rose-100 border-2 border-rose-700'>Sign in</button>
+                <button className='px-4 py-2 rounded-full font-bold uppercase text-rose-100 bg-rose-700'>Sign up</button>
                 <div className='flex items-center'>
                     <CartWidget />
                     <span>{cartItems}</span>
