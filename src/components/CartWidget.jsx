5adf1096d066bc239a44e1697b201a2ccd90a93f
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-09-14 16:32:27 -0300

    min fix

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -4,7 +4,7 @@ import { HiShoppingCart } from 'react-icons/hi';
 export default function CartWidget() {
     return (
         <div className='p-4 items-center'>
-            <HiShoppingCart className='text-emerald-900' size={20} />
+            <HiShoppingCart className='text-rose-900' size={20} />
         </div>
     )
 }
