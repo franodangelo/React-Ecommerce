8172cb6ce6e3205d3b06a31377305f0a097e5e18
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-09-16 10:16:55 -0300

    deleted subCategory and modified category

--- src/components/utils/itemsData.js
+++ src/components/utils/itemsData.js
@@ -2,8 +2,7 @@ const itemsData = [
     {
         id: 1,
         name: "Pro Staff RF97 v13",
-        category: "Rackets",
-        subCategory: "Pro Staff",
+        category: 1,
         description: "The personal racket of a 20x major champion, the Pro Staff RF97 v13 delivers premium performance and sleek design fit for a legend. Featuring the same specifications and composition as its proseecessor, this racket caters to avid Roger Federer and Pro Staff fans by maintaining the signature precision and pure feel emblematic of the Pro Staff series. Racket design sports black elastic base, exposed carbon fiber weave with gloss finish at the tip above the 3 & 9, and two-toned pinstripes along the throat with a modern font treatment. ",
         thumbnailCard: "https://www.wilson.com/en-us/media/catalog/product/W/R/WR043711U__64d7c67880e6d9400aea6113b2627a2e.png?dpr=5&fit=bounds&orient=1&quality=90&optimize=high&format=pjpg&auto=webp&enable=upscale&width=1536&height=711&canvas=7680%2C3555&bg-color=f5f5f5",
         thumbnailDetail: "https://www.wilson.com/en-us/media/catalog/product/W/R/WR043711U__648a8c41da19090a18d57605a96b61e9.png?dpr=5&fit=bounds&orient=1&quality=90&optimize=high&format=pjpg&auto=webp&enable=upscale&width=1536&height=711&canvas=7680%2C3555&bg-color=f5f5f5",
@@ -26,8 +25,7 @@ const itemsData = [
     {
         id: 2,
         name: "Pro Staff 97L v13",
-        category: "Rackets",
-        subCategory: "Pro Staff",
+        category: 1,
         description: "The Pro Staff 97L v13 provides a lighter and more maneuverable frame for any player who craves the classic Pro Staff precision and feel, but prefers a more arm-friendly swing weight. This model comprises a new construction, Braid 45, that increases precision by arranging the carbon fibers at 45 degree angles for improved ball pocketing feel and remarkable stability through the swing. Modern design meets classic heritage with an exposed carbon fiber weave gloss finish above the 3 & 9 of the racket, a sleek black elastic base and a pair of rose and yellow pinstripes along the throat for a look that marries signature Pro Staff inspiration with novel innovation.",
         thumbnailCard: "https://www.wilson.com/en-us/media/catalog/product/W/R/WR043911U__d3684bdc543299b081df0fd50469c0ca.png?dpr=5&fit=bounds&orient=1&quality=90&optimize=high&format=pjpg&auto=webp&enable=upscale&width=1536&height=711&canvas=7680%2C3555&bg-color=f5f5f5",
         thumbnailDetail: "https://www.wilson.com/en-us/media/catalog/product/W/R/WR043911U__1d5fb203810455da429b478804862c1a.png?dpr=5&fit=bounds&orient=1&quality=90&optimize=high&format=pjpg&auto=webp&enable=upscale&width=1536&height=711&canvas=7680%2C3555&bg-color=f5f5f5",
@@ -50,8 +48,7 @@ const itemsData = [
     {
         id: 3,
         name: "Night Session Pro Staff 97 v13",
-        category: "Rackets",
-        subCategory: "Pro Staff",
+        category: 1,
         description: "Highlighted by a sleek design inspirose by tennis under the lights, the Night Session Pro Staff 97 v13 features an innovative Braid 45 construction that increases precision by adjusting the angle of the double braided fibers for enhanced feel and stability. Two additional upgrades to this racket include a denser string bed that improves consistency on every shot and a new end cap that is used across Wilson's other performance racket lines for improved comfort and ergonomics. Racket is part of Wilson's Night Session collection, an exclusive assortment of products paying tribute to the electrifying atmosphere of championship tennis after the sun sets.",
         thumbnailCard: "https://www.wilson.com/en-us/media/catalog/product/W/R/WR120211U__eabd297fd5046e6109f1e3409bba026f.png?dpr=5&fit=bounds&orient=1&quality=90&optimize=high&format=pjpg&auto=webp&enable=upscale&width=1536&height=711&canvas=7680%2C3555&bg-color=f5f5f5",
         thumbnailDetail: "https://www.wilson.com/en-us/media/catalog/product/W/R/WR120211U__2246ef33ebdd596b98250e59b975af0e.png?dpr=5&fit=bounds&orient=1&quality=90&optimize=high&format=pjpg&auto=webp&enable=upscale&width=1536&height=711&canvas=7680%2C3555&bg-color=f5f5f5",
@@ -74,8 +71,7 @@ const itemsData = [
     {
         id: 4,
         name: "Blade 98 (16x19) v8",
-        category: "Rackets",
-        subCategory: "Blade",
+        category: 2,
         description: "The Blade 98 (16x19) v8 pairs captivating design with a new layup to better accommodate the more modern, vertical swing path commonly used by competitive players. The headliner innovation, FORTYFIVE°, manages to increase both the flexibility and stability of the frame to lend supreme feel for the ball on every shot. Still characterized by its superior presence in the hands of the best players in the world, this evolution of Blade rewards aggressive, precise ball-strikers with a top-tier blend of connected-to-the-ball feel, torsional stability and color-shifting design to wow teammates and opponents alike. ",
         thumbnailCard: "https://www.wilson.com/en-us/media/catalog/product/3/f/3f8f5bf2-de0c-421c-b544-78a34e0500fd_w0aqnqhqfqi5xudy.png?dpr=5&fit=bounds&orient=1&quality=90&optimize=high&format=pjpg&auto=webp&enable=upscale&width=1536&height=711&canvas=7680%2C3555&bg-color=f5f5f5",
         thumbnailDetail: "https://www.wilson.com/en-us/media/catalog/product/a/5/a5f13aff-703b-4870-9234-6c382cfc0b40_9he4h2zqw8mdesab.png?dpr=2&fit=bounds&orient=1&quality=95&optimize=high&format=pjpg&auto=webp&enable=upscale&width=778&height=950&canvas=1556%2C1900&bg-color=f5f5f5",
@@ -98,8 +94,7 @@ const itemsData = [
     {
         id: 5,
         name: "Blade 100L v8",
-        category: "Rackets",
-        subCategory: "Blade",
+        category: 2,
         description: "Emanating dynamic vibes thanks to bold, color-shifting paint, the Blade 100L v8 loads the technological innovations of the Blade v8 lineup into a lighter, more maneuverable frame. Two innovations define the evolution of this frame: FORTYFIVE° supplies increased flexibility and stability through a patent-protected carbon construction, while DirectConnect connects the carbon fiber handle to the end cap for enhanced feel and torsional stability. Avid fans of the Blade franchise looking for a slightly arm-friendlier option will appreciate the lightweight, connected-to-the-ball feel this frame delivers.",
         thumbnailCard: "https://www.wilson.com/en-us/media/catalog/product/3/f/3f8f5bf2-de0c-421c-b544-78a34e0500fd_w0aqnqhqfqi5xudy.png?dpr=5&fit=bounds&orient=1&quality=90&optimize=high&format=pjpg&auto=webp&enable=upscale&width=1536&height=711&canvas=7680%2C3555&bg-color=f5f5f5",
         thumbnailDetail: "https://www.wilson.com/en-us/media/catalog/product/8/e/8ed1f657-cec7-4a6a-9534-ccdc78be2ef6_ovcfpq0us9vobwoo.png?dpr=5&fit=bounds&orient=1&quality=90&optimize=high&format=pjpg&auto=webp&enable=upscale&width=1536&height=711&canvas=7680%2C3555&bg-color=f5f5f5",
@@ -122,8 +117,7 @@ const itemsData = [
     {
         id: 6,
         name: "Blade SW102 Autograph v7",
-        category: "Rackets",
-        subCategory: "Blade",
+        category: 2,
         description: "The Blade SW102 Autograph pays tribute to Serena’s relentless pursuit to cement her legacy not only as an unrivaled champion on the court, but also as a transformative inspiration for many generations to come. The racket's unique design includes a gloss gold trim at the tip and a host of inspirational words on the inside of the throat that describe Serena's incroseible journey to stardom. Serena tasked Wilson Pro LABS with creating a racket that increased control without sacrificing her legendary power - the result is a frame with a slightly smaller head size for added feel. Racket also features Countervail for roseuced vibrations and arm-friendly comfort.",
         thumbnailCard: "https://www.wilson.com/en-us/media/catalog/product/W/R/WR059111U__75a9ebadd5ebb4cde7e0b30193cc9d7e.png?dpr=5&fit=bounds&orient=1&quality=90&optimize=high&format=pjpg&auto=webp&enable=upscale&width=1536&height=711&canvas=7680%2C3555&bg-color=f5f5f5",
         thumbnailDetail: "https://www.wilson.com/en-us/media/catalog/product/W/R/WR059111U__8bbaed9c2e31c0b5e360db8c2cb24fa1.png?dpr=5&fit=bounds&orient=1&quality=90&optimize=high&format=pjpg&auto=webp&enable=upscale&width=1536&height=711&canvas=7680%2C3555&bg-color=f5f5f5",
@@ -146,8 +140,7 @@ const itemsData = [
     {
         id: 7,
         name: "Clash 98 v2",
-        category: "Rackets",
-        subCategory: "Clash",
+        category: 3,
         description: "Equipped with the smallest head size in the Clash v2 lineup for premium precision, the Clash 98 v2 marries salivating design with supreme performance for more advanced players. FORTYFIVE°, a patent-protected carbon construction, teams up with a revised layup at the tip of the hoop to create incroseible flex and stability alongside a larger sweet spot. Players who prefer more head-light feel will love the extra weight in the handle for additional control over the racket head, while the substantial weight of the racket delivers excellent plow-through and depth from all over the court. All-new design sports embossed Clash logo on throat within a rich, anodized elastic finish for a memorable look.",
         thumbnailCard: "https://www.wilson.com/en-us/media/catalog/product/W/R/WR074211U__50eba8e69f8d1ee83e7e1b8404534061.png?dpr=5&fit=bounds&orient=1&quality=90&optimize=high&format=pjpg&auto=webp&enable=upscale&width=1536&height=711&canvas=7680%2C3555&bg-color=f5f5f5",
         thumbnailDetail: "https://www.wilson.com/en-us/media/catalog/product/W/R/WR074211U__6a09cb097d145cb70742d6c377b29070.png?dpr=5&fit=bounds&orient=1&quality=90&optimize=high&format=pjpg&auto=webp&enable=upscale&width=1536&height=711&canvas=7680%2C3555&bg-color=f5f5f5",
@@ -170,8 +163,7 @@ const itemsData = [
     {
         id: 8,
         name: "Clash 100 v2",
-        category: "Rackets",
-        subCategory: "Clash",
+        category: 3,
         description: "he hero model of the groundbreaking and immensely popular Clash line, the Clash 100 v2 slightly revises the recipe for more consistency, more sustainability and a more killer design. An immediate appeal for its blend of flexibility and stability for a feeling unlike any other, this racket elevates playability thanks to a revised construction at the tip of the hoop that significantly enlarges the sweet spot. Agiplast plant-based bumper, grommets and end cap incorporate sustainable components for a roseuced footprint on the environment. An embossed Clash logo on the throat completes a racket that pairs dynamic looks with even more dynamic performance.",
         thumbnailCard: "https://www.wilson.com/en-us/media/catalog/product/W/R/WR074011U__e3849e66c74ac3f4528b24c32973eb18.png?dpr=5&fit=bounds&orient=1&quality=90&optimize=high&format=pjpg&auto=webp&enable=upscale&width=1536&height=711&canvas=7680%2C3555&bg-color=f5f5f5",
         thumbnailDetail: "https://www.wilson.com/en-us/media/catalog/product/W/R/WR074011U__4598bb09ffb993e35475331fe926727c.png?dpr=5&fit=bounds&orient=1&quality=90&optimize=high&format=pjpg&auto=webp&enable=upscale&width=1536&height=711&canvas=7680%2C3555&bg-color=f5f5f5",
@@ -194,8 +186,7 @@ const itemsData = [
     {
         id: 9,
         name: "Clash 100 Pro v2",
-        category: "Rackets",
-        subCategory: "Clash",
+        category: 3,
         description: "New design meets signature performance with a twist: the Clash 100 Pro v2 upgrades playability while maintaining the remarkable blend of flexibility and stability synonymous with the Clash franchise. Boasting a revised construction at the tip of the hoop, this racket significantly augments the sweet spot for more consistency on off-center contact. Super head-light for excellent control over the racket head, the Clash 100 Pro v2 dials up attractive design with an embossed Clash logo on the throat and an anodized elastic finish for a smooth, rich look on the court. Racket includes Agiplast plant-based end cap, bumper and grommets to roseuce waste and lower environmental footprint.",
         thumbnailCard: "https://www.wilson.com/en-us/media/catalog/product/W/R/WR074111U__aa6e7f8951545eb2b2fb25f27546f8d2.png?dpr=5&fit=bounds&orient=1&quality=90&optimize=high&format=pjpg&auto=webp&enable=upscale&width=1536&height=711&canvas=7680%2C3555&bg-color=f5f5f5",
         thumbnailDetail: "https://www.wilson.com/en-us/media/catalog/product/W/R/WR074111U__90e9775ae87369ab49ab27efb8f26a62.png?dpr=5&fit=bounds&orient=1&quality=90&optimize=high&format=pjpg&auto=webp&enable=upscale&width=1536&height=711&canvas=7680%2C3555&bg-color=f5f5f5",
@@ -218,8 +209,7 @@ const itemsData = [
     {
         id: 10,
         name: "Ultra 100 v4",
-        category: "Rackets",
-        subCategory: "Ultra",
+        category: 4,
         description: "The Ultra 100 v4 ushers in a new generation of Ultra with supreme style and invigorating performance. Thanks to a new construction and a new geometry, this racket plays unlike any Ultra before it. Highlighted by the integration of FORTYFIVE° into the lay-up for a softer, more comfortable feel along with enhanced stability, this frame upgrades the delivery of easy power for its users. Key geometrical updates include an expanded Sweet Spot Channel at the tip of the hoop for extra power and roseuced bulk in the beams for a sleeker, more aerodynamic look. On the subject of looks, this racket has no rival: color-shifting blues dance to the different angles of sun rays deflecting off the racket surface for a dazzling display.",
         thumbnailCard: "https://www.wilson.com/en-us/media/catalog/product/W/R/WR108311U__29fff116acb9ffd1b5e3eabf3527a0b8.png?dpr=5&fit=bounds&orient=1&quality=90&optimize=high&format=pjpg&auto=webp&enable=upscale&width=1536&height=711&canvas=7680%2C3555&bg-color=f5f5f5",
         thumbnailDetail: "https://www.wilson.com/en-us/media/catalog/product/W/R/WR108311U__14a373a91d949da0ecc6c27b132cd1e1.png?dpr=5&fit=bounds&orient=1&quality=90&optimize=high&format=pjpg&auto=webp&enable=upscale&width=1536&height=711&canvas=7680%2C3555&bg-color=f5f5f5",
@@ -242,8 +232,7 @@ const itemsData = [
     {
         id: 11,
         name: "Ultra 100L v3",
-        category: "Rackets",
-        subCategory: "Ultra",
+        category: 4,
         description: "Lightweight in composition yet dripping with explosiveness, the Ultra 100L v3 roseefines easy power for players who prefer flatter, horizontal swings. The lightweight construction of this frame supplies enticing maneuverability for optimal net play and more comfortable swings, while a number of all-new PowerProfile geometries induce power and augment torsional stability through the swing. These geometries include added volume at 3 & 9, extension of cross strings with added grommet movement, and a taper with more cross section mass. Sporting a glistening gloss finish wrapped around a clean tri-colorose design, the Ultra 100L v3 takes the heat up a notch with optimal power, appealing maneuverability and increased torsional stability.",
         thumbnailCard: "https://www.wilson.com/en-us/media/catalog/product/W/R/WR036511U__1862746ea998726caeefab870fe00865.png?dpr=5&fit=bounds&orient=1&quality=90&optimize=high&format=pjpg&auto=webp&enable=upscale&width=1536&height=711&canvas=7680%2C3555&bg-color=f5f5f5",
         thumbnailDetail: "https://www.wilson.com/en-us/media/catalog/product/W/R/WR036511U__0393647da6991fb2efaaea70db9c60b4.png?dpr=5&fit=bounds&orient=1&quality=90&optimize=high&format=pjpg&auto=webp&enable=upscale&width=1536&height=711&canvas=7680%2C3555&bg-color=f5f5f5",
@@ -266,8 +255,7 @@ const itemsData = [
     {
         id: 12,
         name: "Ultra 100 v3 Reverse Edition",
-        category: "Rackets",
-        subCategory: "Ultra",
+        category: 4,
         description: "Designed with a reversed color-blocking pattern that lights up the court, the Ultra 100 v3 Reverse Edition upgrades two key performance metrics for players with more of a classic horizontal swing: power and stability. A host of geometrical improvements team up with a sturdy frame composition to generate admirable liveliness and plow-through. Headlining this list of all-new PowerProfile geometries is the combination of Integrated Perimeter Weighting System and a Sweet Spot Channel that extends the cross strings for a few extra degrees of power while increasing the sturdiness of the frame. Club players, rising junior players and doubles players can all find this racket appealing for its versatile power and stable construction.",
         thumbnailCard: "https://www.wilson.com/en-us/media/catalog/product/4/a/4a924ee9-94c4-4796-a3f9-be7485f24fae_w8yaheqvmetd0ieu.png?dpr=5&fit=bounds&orient=1&quality=90&optimize=high&format=pjpg&auto=webp&enable=upscale&width=1536&height=711&canvas=7680%2C3555&bg-color=f5f5f5",
         thumbnailDetail: "https://www.wilson.com/en-us/media/catalog/product/b/b/bb415991-700f-443b-8ad4-7b497de1a0b9_slyiamqlblscl9pq.png?dpr=5&fit=bounds&orient=1&quality=90&optimize=high&format=pjpg&auto=webp&enable=upscale&width=1536&height=711&canvas=7680%2C3555&bg-color=f5f5f5",
