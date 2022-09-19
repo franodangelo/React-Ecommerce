ddde1ba26f99195ac92c0a8e742ff45cbcc42d32
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-09-19 14:29:40 -0300

    specs and itemcount; responsive ok

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -20,13 +20,13 @@ export default function ItemDetail({ item }) {
 
     return (
         <section className="overflow-hidden h-auto">
-            <div className="flex flex-col container px-4 py-8 mx-auto gap-8">
+            <div className="flex flex-col container px-4 py-4 mx-auto gap-8">
                 <div className="lg:w-4/5 mx-auto flex flex-wrap">
                     <img className="lg:w-1/2 w-full object-cover object-center rounded border border-stone-200"
                         src={item.thumbnailDetail}
                         alt="product"
                     />
-                    <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+                    <div className="lg:w-1/2 w-full lg:pl-8 lg:py-2 mt-6 lg:mt-0">
                         <h2 className="text-sm text-stone-500 tracking-widest">
                             Rackets / {
                                 item.category === 1 ? "Pro Staff" :
@@ -38,7 +38,7 @@ export default function ItemDetail({ item }) {
                         <div className="flex justify-between items-center">
                             <h1 className="py-2 font-bold text-3xl uppercase text-stone-900">{item.name}</h1>
                         </div>
-                        <div className="flex py-2 items-center">
+                        <div className="flex items-center">
                             <span className="flex items-center gap-4">
                                 <div className="flex gap-1 items-center">
                                     {item.rate === 0 ?
@@ -50,30 +50,55 @@ export default function ItemDetail({ item }) {
                                             viewBox="0 0 24 24">
                                             <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
                                         </svg>}
-                                    <p className="text-center text-rose-700">{item.rate > 0 ? item.rate : null}</p>
+                                    <p className="text-center text-rose-700">
+                                        {item.rate > 0 ? item.rate : <p className="text-stone-400">0</p>}
+                                    </p>
                                 </div>
-                                <p className="text-center text-stone-600">{item.ratings} {item.ratings > 1 ? "reviews" : "review"}</p>
+                                <p className="text-center text-stone-600">
+                                    {
+                                        item.ratings === 0 ? "No reviews" :
+                                            item.ratings > 1 ? <p>{item.ratings} reviews</p> :
+                                                <p>{item.ratings} review</p>
+                                    }
+                                </p>
                             </span>
                         </div>
-                        <p className="tracking-tighter">{item.description}</p>
+                        <p className="py-2 tracking-tighter">{item.description}</p>
                         <div className="flex items-center border-b-2 border-stone-200">
                             <div className="py-4 flex items-center">
                                 <span className="mr-4">Size</span>
                                 <div className="relative">
-                                    <select className="p-2 rounded-md border border-stone-400 focus:border-rose-500">
+                                    <select className="p-2 rounded-md border border-stone-400">
                                         <option>{item.size}</option>
                                     </select>
                                 </div>
                             </div>
                         </div>
+                        {/*Price section */}
                         <div className="flex flex-col py-4 gap-2">
                             <div className="flex items-center gap-2">
-                                {item.discountPercentage ? <p className="font-bold text-2xl line-through">${item.price.toFixed(2)}</p> : null}
-                                <p className="font-bold text-2xl text-rose-700">${item.discountPercentage != null ? ((100 - item.discountPercentage) * item.price / 100) : item.price}</p>
-                                {item.discountPercentage ? <p className="text-stone-600">-{item.discountPercentage}% OFF</p> : null}
+                                {item.discountPercentage != null ? // if item has disc, render the original price w/linethrough
+                                    <p className="font-bold text-2xl line-through">${item.price.toFixed(2)}</p> :
+                                    null
+                                }
+                                <p className="font-bold text-2xl text-rose-700">
+                                    ${item.discountPercentage != null ? // if item has disc, render the new price applying it. If not, render the original price
+                                        ((100 - item.discountPercentage) * item.price / 100) :
+                                        (item.price)
+                                    }
+                                </p>
+                                {item.discountPercentage != null ? // if item has disc, render the discPercentage
+                                    <p className="text-stone-600">-{item.discountPercentage}% OFF</p> :
+                                    null
+                                }
                             </div>
-                            <p>{item.interestFreePayments} interest free payments of ${(item.price / item.interestFreePayments).toFixed(2)}</p>
+                            <p>
+                                {item.interestFreePayments} interest free payments of
+                                ${(item.price / item.interestFreePayments).toFixed(2) // amount of each payment
+                                }
+                            </p>
                         </div>
+                        {/* Buttons section */}
                         {added === false ?
                             (<ItemCount stock={itemStock} initial={itemCount} onAdd={onAdd} />) :
                             (<Link to="/cart">
@@ -85,16 +110,15 @@ export default function ItemDetail({ item }) {
                         }
                     </div>
                 </div>
+                {/* Specifications section */}
                 <section className="lg:w-4/5 mx-auto flex flex-col flex-wrap gap-4">
                     <h1 className="py-2 font-bold text-xl text-stone-900 uppercase">Specifications</h1>
-                    <div className="flex flex-col gap-4">
-                        <div>
-                            <h2 className="text-lg">Head (sq in):</h2>
-                        </div>
-                        <h2 className="text-lg">Length (in):</h2>
-                        <h2 className="text-lg">String Pattern:</h2>
-                        <h2 className="text-lg">Strung Weight (grams):</h2>
-                        <h2 className="text-lg">Strung balance (cm):</h2>
+                    <div className="flex flex-col gap-2 divide-y-2 divide-stone-200">
+                        <h2 className="text-lg">Head: {item.head} sq in</h2>
+                        <h2 className="text-lg">Length: {item.length} in</h2>
+                        <h2 className="text-lg">String pattern: {item.stringPattern}</h2>
+                        <h2 className="text-lg">Strung weight: {item.strungWeight}g</h2>
+                        <h2 className="text-lg">Strung balance: {item.strungBalance}cm</h2>
                     </div>
                 </section>
             </div>
