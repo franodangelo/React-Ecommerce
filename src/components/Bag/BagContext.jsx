bd68b1f7a686676f52048a31cfdf01e5d696d695
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-09-23 13:30:44 -0300

    new functions

--- src/components/Bag/BagContext.jsx
+++ src/components/Bag/BagContext.jsx
@@ -5,9 +5,40 @@ export default function BagContextProvider({ children }) {
 
     const [bagList, setBagList] = useState([]);
 
-    function addItem(item, count) {
-        let itemForCart = { ...item, count };
-        setBagList([...bagList, itemForCart]);
+    function isOnCart(id) {
+        const itemOnCart = bagList.find(i => i.id === id);
+        return itemOnCart ? true : false;
+    }
+
+    const addItem = (item, qty) => {
+        if (isOnCart(item.id)) {
+            setBagList((prevState) => {
+                return prevState.map(i => {
+                    if (i.id === item.id) {
+                        if (i.quantity + qty > i.stock - 1) {
+                            alert(`Sorry, the maximum stock of this product is ${i.stock}`)
+                            return {
+                                ...i,
+                                quantity: i.stock
+                            }
+                        } else {
+                            return {
+                                ...i,
+                                quantity: i.quantity + qty
+                            }
+                        }
+                    } else {
+                        return i;
+                    }
+                })
+            })
+        } else {
+            let itemForCart = {
+                ...item,
+                quantity: qty
+            }
+            setBagList((prevState) => [...prevState, itemForCart]);
+        }
     }
 
     function removeItem(id) {
@@ -19,8 +50,20 @@ export default function BagContextProvider({ children }) {
         setBagList([]);
     }
 
+    function priceBag() {
+        return Math.round(
+            bagList.reduce((totalPrice, item) => totalPrice + item.price * item.quantity, 0)
+        )
+    }
+
+    function quantityBag() {
+        return Math.round(
+            bagList.reduce((totalItems, item) => totalItems + item.quantity, 0)
+        )
+    }
+
     return (
-        <BagContext.Provider value={{ bagList, addItem, removeItem, clearBag }}>
+        <BagContext.Provider value={{ bagList, addItem, removeItem, clearBag, priceBag, quantityBag }}>
             {children}
         </BagContext.Provider>
     )
