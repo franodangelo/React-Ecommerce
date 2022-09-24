2b14fdc00714f33d4ca4f189f329bb8500693a85
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-09-24 20:34:22 -0300

    better layout. not responsive yet

--- src/components/Bag/BagItem.jsx
+++ src/components/Bag/BagItem.jsx
@@ -6,14 +6,16 @@ export default function BagItem(props) {
 
     return (
         <main className="flex w-full max-h-40 justify-between items-center rounded shadow">
-            <img className="w-80" src={props.thumbnailCard} alt="" />
-            <section className="flex justify-start">
-                <div className="flex gap-4">
-                    <h2 className="">{props.quantity}x</h2>
-                    <h1 className="font-bold">{props.name}</h1>
-                </div>
-                <h1>Total price: ${(props.price * props.quantity).toFixed(2)}</h1>
-            </section>
+            <div className="flex items-center">
+                <img className="w-80" src={props.thumbnailCard} alt="item thumbnail" />
+                <section className="flex flex-col ml-4 gap-2">
+                    <div className="flex gap-4 items-center">
+                        <span className="text-sm">{props.quantity}x</span>
+                        <h1 className="font-bold text-xl">{props.name}</h1>
+                    </div>
+                    <h1>Total price: <strong className="text-rose-700">${(props.price * props.quantity).toFixed(2)}</strong></h1>
+                </section>
+            </div>
             <button className="secondaryBtn mr-4"
                 onClick={() => { context.removeItem(props.id) }}>
                 Remove from bag
