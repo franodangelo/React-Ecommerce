fa8946422bb52eee330b97d8905e8452bfbbfb7b
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-09-26 13:46:51 -0300

    get filtered items and singular item for itemDetail

--- src/utils/fbConfig.js
+++ src/utils/fbConfig.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { getFirestore, collection, doc, getDoc, getDocs, query, where } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -18,11 +18,35 @@ const app = initializeApp(firebaseConfig);
 // Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
 
-export async function fbFetch() {
-    const fbData = await getDocs(collection(db, "item"));
+export async function fbFetch(category) {
+    let q = query(collection(db, "item"));
+    if (category) {
+        q = query(
+            collection(db, "item"),
+            where("category", "==", category)
+        );
+    } else {
+        q = query(collection(db, "item"));
+    }
+    const fbData = await getDocs(q);
     const mappedFbData = fbData.docs.map(d => ({
         id: d.id,
         ...d.data()
     }))
     return mappedFbData;
 }
+
+export async function fbFetchSingularDoc(id) {
+    const docRef = doc(db, "item", id);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+        const item = {
+            id: docSnap.id,
+            ...docSnap.data()
+        }
+        return item;
+    } else {
+        console.log("No such document!");
+    }
+};
