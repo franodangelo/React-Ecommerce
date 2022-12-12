d2adbbb6da6f64209223c8eeb09d408cfe40a163
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-12-12 11:29:59 -0300

    footer ok

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 
 export default function Footer() {
     return (
-        <footer className="flex flex-col md:flex-row w-full h-[120px] mt-8 px-8 py-4 justify-between items-center bg-rose-700">
-            <section className="flex items-center gap-8">
+        <footer className="flex flex-col md:flex-row w-full min-h-[120px] mt-8 px-8 py-4 justify-between items-center bg-rose-700">
+            <section className="flex flex-col md:flex-row items-center gap-2 md:gap-6">
                 <Link to="/">
                     <h1 className="font-bold text-lg text-white uppercase">Advantage.</h1>
                 </Link>
-                <ul className="flex justify-between gap-4">
+                <ul className="flex justify-between gap-4 md:gap-6">
                     <Link to="/prostaff">
                         <li className="font-bold text-xs uppercase cursor-pointer">Pro Staff</li>
                     </Link>
@@ -23,7 +23,7 @@ export default function Footer() {
                     </Link>
                 </ul>
             </section>
-            <section className="flex flex-row md:flex-col items-center gap-4 md:gap-0 md:items-start">
+            <section className="flex flex-row md:flex-col items-center md:items-start gap-4 md:gap-0 text-xs">
                 <h2 className="text-white">All rights reserved</h2>
                 <h3 className="font-bold text-xs md:text-sm text-white">Franco D'Angelo - 2022</h3>
             </section>
