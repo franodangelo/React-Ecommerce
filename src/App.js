f58bd5faa1e7f8a0bb71b7ed6b8a7feea2bb9b13
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-12-12 10:24:18 -0300

    router optimized with lazy and suspense

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import ItemListContainer from "./components/Catalog/ItemListContainer";
-import ItemDetailContainer from "./components/Catalog/ItemDetailContainer";
-import Bag from "./components/Bag/Bag";
+
+// import ItemListContainer from "./components/Catalog/ItemListContainer";
+// import ItemDetailContainer from "./components/Catalog/ItemDetailContainer";
+// import Bag from "./components/Bag/Bag";
 import Footer from "./components/Footer";
 import BagContextProvider from "./components/Bag/BagContext";
+
+const ItemListContainer = lazy(() => import("./components/Catalog/ItemListContainer"));
+const ItemDetailContainer = lazy(() => import("./components/Catalog/ItemDetailContainer"));
+const Bag = lazy(() => import("./components/Bag/Bag"));
 
 export default function App() {
   return (
     <BagContextProvider>
       <BrowserRouter>
         <Navbar />
-        <Routes>
-          <Route exact path="/" element={<ItemListContainer />} />
-          <Route exact path="/:category" element={<ItemListContainer />} />
-          <Route exact path="/:category/:id" element={<ItemDetailContainer />} />
-          <Route path='/bag' element={<Bag />} />
-        </Routes>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Routes>
+            <Route exact path="/" element={<ItemListContainer />} />
+            <Route exact path="/:category" element={<ItemListContainer />} />
+            <Route exact path="/:category/:id" element={<ItemDetailContainer />} />
+            <Route path='/bag' element={<Bag />} />
+          </Routes>
+        </Suspense>
         <Footer />
       </BrowserRouter>
     </BagContextProvider>
